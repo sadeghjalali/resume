2252c3dc60112e2c0237d399d637f75bedd18abb
sadeghjalali <s.jalali.tsu@gmail.com>
2019-03-16 13:11:19 +0330

    Updates

---
+++ precache-manifest.053fd660d27b78f5bda6cbc87a9409da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.699f89d9.js"
+  },
+  {
+    "revision": "5fedd02249242850ef17",
+    "url": "/resume/static/js/main.5fedd022.chunk.js"
+  },
+  {
+    "revision": "5a89b27bbba678df164f",
+    "url": "/resume/static/js/2.5a89b27b.chunk.js"
+  },
+  {
+    "revision": "5fedd02249242850ef17",
+    "url": "/resume/static/css/main.b40192df.chunk.css"
+  },
+  {
+    "revision": "0125dcdc197ed8badf051efed4f934d6",
+    "url": "/resume/index.html"
+  }
+];
